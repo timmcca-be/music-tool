b41ebfc7025a33a7e260eb0ce6e1c5e147e79b91
Tim McCabe <me@timmcca.be>
2020-05-21 15:51:02 -0400

    Use hash history (works with GH pages)

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import { h } from 'preact';
 import { Router } from 'preact-router';
+import { createHashHistory } from 'history';
 
 import Main from '../routes/main/main';
 import Scales from '../routes/scales/scales';
 
 const App = () => (
-	<Router>
+	<Router history={createHashHistory()}>
 		<Main path="/" />
 		<Scales path="/scales/:keyCenter" />
 	</Router>
