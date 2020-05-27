53e8f1d1ef631cdd62e4224d658ad12404c90f2a
Tim McCabe <me@timmcca.be>
2020-05-27 19:08:01 -0400

    Fix drone on back button

--- src/components/toggle/droneToggle.js
+++ src/components/toggle/droneToggle.js
@@ -1,22 +1,22 @@
 import { h } from 'preact';
 import { playDrone, stopDrone } from '../../utils/playChord';
-import { useState } from 'preact/hooks';
+import { useState, useEffect } from 'preact/hooks';
 import Toggle from './toggle';
 
 function DroneToggle({ note, semitone }) {
     const [droneEnabled, setDroneEnabled] = useState(false);
 
-    const toggleDrone = enabled => {
-        setDroneEnabled(enabled);
-        if(enabled) {
+    useEffect(() => {
+        if(droneEnabled) {
             playDrone(semitone);
         } else {
             stopDrone();
         }
-    }
+        return stopDrone;
+    }, [droneEnabled, semitone]);
 
     return (
-        <Toggle name={`${note} drone`} enabled={droneEnabled} setEnabled={toggleDrone} keyboardShortcut="o">
+        <Toggle name={`${note} drone`} enabled={droneEnabled} setEnabled={setDroneEnabled} keyboardShortcut="o">
             {note}<span class="desktop"> drone</span>
         </Toggle>
     );
