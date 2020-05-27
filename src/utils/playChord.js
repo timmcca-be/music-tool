616cc7614b5be7a7bda268acf3d33b89c90a7c20
Tim McCabe <me@timmcca.be>
2020-05-26 21:06:10 -0400

    Fix ramps on Firefox and WebKit

--- src/utils/playChord.js
+++ src/utils/playChord.js
@@ -20,7 +20,9 @@ function playTone(hz) {
 
 function stopChord() {
     if(gainNode !== null) {
-        gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
+        // see comment in playChord
+        gainNode.gain.linearRampToValueAtTime(gainNode.gain.value, audioCtx.currentTime);
+        gainNode.gain.linearRampToValueAtTime(0, audioCtx.currentTime + 0.05);
     }
 }
 
@@ -28,9 +30,15 @@ function playChord(semitones) {
     stopChord();
     gainNode = audioCtx.createGain();
     gainNode.connect(audioCtx.destination);
-    gainNode.gain.value = 1 / semitones.length;
+    gainNode.gain.value = 0;
     semitones.forEach(semitone => playTone(440 * Math.pow(2, (semitone - 9) / 12)));
-    gainNode.gain.linearRampToValueAtTime(0, audioCtx.currentTime + 1);
+    // this is a hack to make the ramps work on Firefox and WebKit.
+    // the first ramp does not work, so we add a useless one here.
+    // once this: https://bugzilla.mozilla.org/show_bug.cgi?id=1171438
+    //    is resolved and the issue is fixed in WebKit, this line can be removed
+    gainNode.gain.linearRampToValueAtTime(0, audioCtx.currentTime);
+    gainNode.gain.linearRampToValueAtTime(1 / semitones.length, audioCtx.currentTime + 0.03);
+    gainNode.gain.linearRampToValueAtTime(0, audioCtx.currentTime + 1.1);
 }
 
 export { playChord, stopChord };
