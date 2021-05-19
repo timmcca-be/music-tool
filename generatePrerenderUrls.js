04263488db9e75f3cee98e3d8f7c25c8a891582b
Tim McCabe <me@timmcca.be>
2021-05-19 11:49:18 -0400

    Update dependencies and fix prerendering

---
+++ generatePrerenderUrls.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const urls = [{
+    url: '/',
+    title: 'Chord Scales'
+}];
+
+const notes = ['C', 'G', 'D', 'A', 'E', 'B', 'Fs', 'Db', 'Ab', 'Eb', 'Bb', 'F'];
+for(const note of notes) {
+    urls.push({
+        url: `/scales/${note}`,
+        title: `${note.replace('s', '♯').replace('b', '♭')} Scales`
+    })
+}
+
+fs.writeFileSync('prerender-urls.json', JSON.stringify(urls, null, 4));
--- src/components/app.js
+++ src/components/app.js
@@ -1,21 +1,12 @@
 import { h } from 'preact';
 import { Router } from 'preact-router';
-import { createHashHistory } from 'history';
 
 import Main from '../routes/main/main';
 import Scales from '../routes/scales/scales';
 
 function App() {
-	if(typeof window === 'undefined') {
-		return (
-			<Router>
-				<Main path="/" />
-				<Scales path="/scales/:keyCenter" />
-			</Router>
-		);
-	}
 	return (
-		<Router history={createHashHistory()}>
+		<Router>
 			<Main path="/" />
 			<Scales path="/scales/:keyCenter" />
 		</Router>
