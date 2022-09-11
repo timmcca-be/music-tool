65147f774fa4b694e96cabd98d77fdfa1adc077b
Tim McCabe <timmccabe@khanacademy.org>
2022-09-11 17:18:34 -0400

    Make it so chords sustain as long as the button is held down

--- src/utils/playChord.js
+++ src/utils/playChord.js
@@ -25,15 +25,14 @@ function playDrone(semitone) {
 }
 
 let gainNode = null;
-function playSemitone(semitone, currentGainNode, duration) {
+// returns a callback to end the semitone
+function playSemitone(semitone, currentGainNode) {
     const oscillator = audioCtx.createOscillator();
     oscillator.type = 'triangle';
     oscillator.frequency.value = 440 * Math.pow(2, (semitone - 9) / 12);
     oscillator.connect(currentGainNode);
     oscillator.start();
-    if(duration !== undefined) {
-        oscillator.stop(audioCtx.currentTime + duration);
-    }
+    return () => oscillator.stop(audioCtx.currentTime + 2);
 }
 
 function stopGainNode(currentGainNode, duration) {
@@ -47,19 +46,25 @@ function stopGainNode(currentGainNode, duration) {
 
 const stopChord = () => stopGainNode(gainNode, 0.05);
 
+// returns a callback to end the chord
 function playChord(semitones) {
     stopChord();
     gainNode = audioCtx.createGain();
     gainNode.connect(audioCtx.destination);
     gainNode.gain.value = 0;
-    semitones.forEach(semitone => playSemitone(semitone, gainNode, 2));
+    const semitoneEndFunctions = semitones.map(semitone => playSemitone(semitone, gainNode, 2));
     // this is a hack to make the ramps work on Firefox and WebKit.
     // the first ramp does not work, so we add a useless one here.
     // once this: https://bugzilla.mozilla.org/show_bug.cgi?id=1171438
     //    is resolved and the issue is fixed in WebKit, this line can be removed
     gainNode.gain.linearRampToValueAtTime(0, audioCtx.currentTime);
-    gainNode.gain.linearRampToValueAtTime((droneGainNode === null ? 1 : 0.7) / semitones.length, audioCtx.currentTime + 0.03);
-    gainNode.gain.linearRampToValueAtTime(0, audioCtx.currentTime + 1.1);
+    const volume = (droneGainNode === null ? 1 : 0.7) / semitones.length;
+    gainNode.gain.linearRampToValueAtTime(volume, audioCtx.currentTime + 0.03);
+    return () => {
+        gainNode.gain.linearRampToValueAtTime(volume, audioCtx.currentTime);
+        gainNode.gain.linearRampToValueAtTime(0, audioCtx.currentTime + 1.1);
+        semitoneEndFunctions.forEach((end) => end());
+    };
 }
 
 export { playChord, stopChord, playDrone, stopDrone };
